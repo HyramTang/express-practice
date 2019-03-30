2870277ec8a6291ca00e18a1fe45e705b5fc9f8e
Hyram <hyram.tang@gmail.com>
2019-03-30 23:16:15 +0800

    add upload form

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 var photos = [];
@@ -24,5 +26,11 @@ router.get('/photos', function (req, res, next) {
     photos: photos
   });
 });
+
+router.get('/form', function (req, res) {
+  res.render('photos/upload', {
+    title: 'Photo upload'
+  });
+})
 
 module.exports = router;
